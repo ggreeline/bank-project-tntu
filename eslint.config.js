b8d3bf4127fd8de9f9d778e11ad46c52eaa94169
ggreeline <yara.konoshenko@gmail.com>
2024-12-02 21:06:43 +0200

    clear brench

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import pluginJs from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-
-export default [
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  eslintPluginPrettierRecommended,
-];
